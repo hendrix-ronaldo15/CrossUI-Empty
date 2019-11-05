b9a625b7be98ff05e11cfea9851c12b88a67f3d2
Henry Richard <hr684274@gmail.com>
2019-11-05 14:01:09 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,68 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input5")
+                .setDirtyMark(false)
+                .setLeft("9.166666666666666em")
+                .setTop("6.666666666666667em")
+                .setWidth("28.833333333333332em")
+                .setLabelSize("8em")
+                .setLabelCaption("Link")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input6")
+                .setDirtyMark(false)
+                .setLeft("9.166666666666666em")
+                .setTop("10em")
+                .setWidth("27.166666666666668em")
+                .setLabelSize("8em")
+                .setLabelCaption("Alias")
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input7")
+                .setDirtyMark(false)
+                .setLeft("2.5em")
+                .setTop("16.666666666666668em")
+                .setWidth("47.166666666666664em")
+                .setHeight("12.5em")
+                .setLabelSize("8em")
+                .setLabelCaption("Text Area")
+                .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setDirtyMark(false)
+                .setLeft("17.5em")
+                .setTop("12.5em")
+                .setWidth("18.75em")
+                .setHeight("2.8333333333333335em")
+                .setCaption("Button")
+                .onClick([
+                    {
+                        "desc":"Action 1",
+                        "type":"control",
+                        "target":"xui_ui_input7",
+                        "args":[
+                            "{page.xui_ui_input7.setUIValue()}",
+                            undefined,
+                            "hello"
+                        ],
+                        "method":"setUIValue",
+                        "redirection":"other:callback:call",
+                        "event":1
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
